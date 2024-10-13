edbb12a4243915fde404f3ab4ebd0d9468631a1d
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2024-10-13 13:41:26 +0200

    week 9

--- Chapter6/Course/04_list_iteration.py
+++ Chapter6/Course/04_list_iteration.py
@@ -1,1 +1,5 @@
 my_list = [1, 2, 3, 4, 5]
+a = 0
+while a < len(my_list):
+    print(my_list[a])
+    a += 1
---
+++ Chapter6/Course/06_tuple.py
@@ -0,0 +1,9 @@
+t1 = (1, 2, 'hello', ['leo', 'laurent'])
+print(t1[2])
+print(t1.index(2))
+print(t1.count('hello'))
+print(len(t1))
+# print(t1[1][0])
+print(t1[3][0])
+t1[3][0] = 'Lucas'
+print(t1)
