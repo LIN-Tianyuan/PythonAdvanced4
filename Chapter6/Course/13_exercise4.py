3977728dd026f4f9496b6fd0da878c991a626195
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2024-11-21 21:47:07 +0800

    week 14

---
+++ Chapter6/Course/13_exercise4.py
@@ -0,0 +1,3 @@
+s = ".nohtyP erdnerppa xuev eJ"
+new_s = s[6:0:-1]
+print(new_s)
---
+++ Chapter6/Course/14_set.py
@@ -0,0 +1,43 @@
+"""
+name = {"Python", "666", "Python", "y6"}
+print(name)
+
+
+my_set = {"Leo", "Laurent"}
+# my_set.add("Kevin")
+# my_set.remove("Leo")
+# element = my_set.pop()
+# print(element)
+my_set.clear()
+print(my_set)
+"""
+"""
+set1 = {1, 2, 3}
+set2 = {1, 5, 6}
+set3 = set1.difference(set2)
+print(set3)
+"""
+
+"""
+set1 = {1, 2, 3}
+set2 = {1, 5, 6}
+set1.difference_update(set2)
+print(set1)
+print(set2)
+"""
+
+"""
+set1 = {1, 2, 3}
+set2 = {1, 5, 6}
+set3 = set1.union(set2)
+print(set3)
+"""
+
+"""
+set1 = {1, 2, 3}
+print(len(set1))
+"""
+
+set1 = {1, 2, 3, 4, 5}
+print(set1)
+
